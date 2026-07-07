462bc339a96a4dfc0f75d40e1e994267d3f53c6a
Denys Roberto Brehmer <bdenysroberto@gmail.com>
2026-07-07 17:09:56 -0300

    Adiciona Fazenda Colônia São Francisco e atualiza portal

---
+++ fazendas_proprias/fazenda_colonia_sao_francisco/data/Matricula_dot_12.js
@@ -0,0 +1,1 @@
+var json_Matricula_dot_12 = {"type":"FeatureCollection","name":"Matricula_dot_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","Matricula":"16.078"},"geometry":{"type":"Point","coordinates":[-51.122120087356073,-27.579514918186373]}}]}
